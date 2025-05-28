c76c80a20a1f146a2c253b23da5ca84193c9bc29
Shittu Muliq <ShittuMuliq@gmail.com>
2025-05-28 09:33:49 +0100

    i added some new changes

--- Rock_Paper_Scissors_game_project/main.py
+++ Rock_Paper_Scissors_game_project/main.py
@@ -1,5 +1,6 @@
 
 '''
+shittu test to github
 Python program that simulates a game of Rock, Paper, Scissors between the user and the computer.
 The program Define the possible choices, 
 Get the user's choice, Generate a random choice for the computer, 
